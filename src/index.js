6aa39a1c5ada053e9914a9a663a17409e5315996
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-01-04 17:52:46 +0000

    first attempt

---
+++ src/index.js
@@ -0,0 +1,68 @@
+
+const SHA256 = require('crypto-js/SHA256');
+
+class Block {
+    constructor(timestamp, data, previousHash = '') {
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.index = -1;
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+
+}
+
+class BlockChain {
+    constructor() {
+        this.chain = [this.genesis()];
+    }
+
+    genesis() {
+        let adam = new Block('01/01/2018', 'Genesis Block', '0');
+        adam.index = 0;
+        return adam;
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.index = this.chain.length;
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+
+        this.chain.push(newBlock);
+    }
+
+    /**
+     * @returns {boolean}
+     */
+    isChainValid() {
+        for (let index = 1; index < this.chain.length; index++) {
+            const currentBlock = this.chain[index];
+            const previousBlock = this.chain[index - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) return false;
+            if(currentBlock.previousHash !== previousBlock.hash) return false;
+
+
+        }
+
+        return true;
+    }
+
+}
+
+
+let sampleChain = new BlockChain();
+
+sampleChain.addBlock(new Block(new Date(), { message: 'My first attempt at Blockchain implementation.' }));
+sampleChain.addBlock(new Block(new Date(), { message: 'New Message' }));
+sampleChain.addBlock(new Block(new Date(), { amount: 4 }));
+
+
